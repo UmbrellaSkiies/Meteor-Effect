78154b197024d968f55cf71e7bd02df94c7f9103
Neo Titebe <Neo.Titebe@ominsure.co.za>
2023-11-16 15:55:34 +0200

    Initial commit

--- pages/components/MeteorPreview.tsx
+++ pages/components/MeteorPreview.tsx
@@ -3,22 +3,22 @@ import { Meteors } from "./components/Meteors";
 
 export const MeteorPreview = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-900">
-      <div className="relative max-w-sm">
+    <div className=" h-[40rem]">
+      <div className=" h-3/4 md:h-1/2 w-3/4  relative max-w-sm">
         <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
-        <div className="relative shadow-xl bg-gray-900 border border-gray-800 px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
+        <div className="relative shadow-xl bg-gray-900 border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
           <div className="h-5 w-5 rounded-full border flex items-center justify-center mb-4 border-gray-500">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              strokeWidth="1.5"
+              stroke-width="1.5"
               stroke="currentColor"
               className="h-2 w-2 text-gray-300"
             >
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
+                stroke-linecap="round"
+                stroke-linejoin="round"
                 d="M4.5 4.5l15 15m0 0V8.25m0 11.25H8.25"
               />
             </svg>
@@ -29,11 +29,11 @@ export const MeteorPreview = () => {
           </h1>
 
           <p className="font-normal text-base text-slate-500 mb-4 relative z-50">
-            I don't know what to write, so I'll just paste something cool here.
-            One more sentence because lorem ipsum is just unacceptable.
+            I don't know what to write so I'll just paste something cool here.
+            One more sentence because lorem ipsum is just unacceptable. 
           </p>
 
-          <button className="border px-4 py-1 rounded-lg !text-sm border-gray-500 text-gray-300">
+          <button className="border px-4 py-1 rounded-lg !text-sm  border-gray-500 text-gray-300">
             Explore &rarr;
           </button>
 
@@ -44,5 +44,3 @@ export const MeteorPreview = () => {
     </div>
   );
 };
-
-export default MeteorPreview;
